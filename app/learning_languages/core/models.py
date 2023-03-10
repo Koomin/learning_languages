b7ea2d9c5f9ca64ea64631220d215527de7c066e
Koomin <bartoszkominko@gmail.com>
2023-03-10 16:49:23 +0100

    Override django user.

--- app/learning_languages/core/models.py
+++ app/learning_languages/core/models.py
@@ -1,5 +1,6 @@
 import uuid as uuid_lib
 
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -10,3 +11,7 @@ class CoreModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class CoreUser(AbstractUser, CoreModel):
+    pass
