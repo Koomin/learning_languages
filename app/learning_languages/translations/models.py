396460e069cd338f6b7c7704777058a6984f0f89
Koomin <bartoszkominko@gmail.com>
2023-03-10 17:13:42 +0100

    Add users to Translation model.

--- app/learning_languages/translations/models.py
+++ app/learning_languages/translations/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from learning_languages.core.models import CoreModel
+from learning_languages.core.models import CoreModel, CoreUser
 from learning_languages.languages.models import Language
 
 
@@ -14,6 +14,7 @@ class Word(CoreModel):
 class Translation(CoreModel):
     original_word = models.ForeignKey(Word, on_delete=models.CASCADE, related_name='translations_original')
     translated_word = models.ForeignKey(Word, on_delete=models.CASCADE, related_name='translations_translated')
+    users = models.ManyToManyField(CoreUser, related_name='translations')
 
     def __str__(self):
         return f'{self.original_word} - {self.translated_word}'
