c34c77158652bc2eae63aa9316af36a3233eaea5
Koomin <bartoszkominko@gmail.com>
2023-03-10 17:21:42 +0100

    Add properties to CoreUser and CoreModel.

--- app/learning_languages/core/models.py
+++ app/learning_languages/core/models.py
@@ -8,10 +8,13 @@ class CoreModel(models.Model):
     uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey('core.CoreUser', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         abstract = True
 
 
 class CoreUser(AbstractUser, CoreModel):
-    pass
+    translated_words = models.IntegerField(default=0)
+    today_translated_word = models.IntegerField(default=0)
+    daily_limit = models.IntegerField(default=50)
