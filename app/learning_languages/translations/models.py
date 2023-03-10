461c10f363cf48aa6dbe27d7384028491eb357ae
Koomin <bartoszkominko@gmail.com>
2023-03-10 17:11:20 +0100

    Add str representation.

--- app/learning_languages/translations/models.py
+++ app/learning_languages/translations/models.py
@@ -7,7 +7,13 @@ class Word(CoreModel):
     word = models.CharField(max_length=255)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.word} ({self.language.code})'
+
 
 class Translation(CoreModel):
     original_word = models.ForeignKey(Word, on_delete=models.CASCADE, related_name='translations_original')
     translated_word = models.ForeignKey(Word, on_delete=models.CASCADE, related_name='translations_translated')
+
+    def __str__(self):
+        return f'{self.original_word} - {self.translated_word}'
