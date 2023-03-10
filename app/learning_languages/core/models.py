ca65bb7e9d9ef5676b9f743a71189a0fed434aed
Koomin <bartoszkominko@gmail.com>
2023-03-10 16:07:23 +0100

    Start app core, create base project model.

---
+++ app/learning_languages/core/models.py
@@ -0,0 +1,11 @@
+import uuid as uuid_lib
+from django.db import models
+
+
+class CoreModel(models.Model):
+    uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
