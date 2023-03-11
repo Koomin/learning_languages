b15f3bc992191173237fb557243701f6fbce6cd2
Koomin <bartoszkominko@gmail.com>
2023-03-11 14:15:07 +0100

    Create serializer and viewset for Language.

--- app/learning_languages/config/routers.py
+++ app/learning_languages/config/routers.py
@@ -1,3 +1,5 @@
+from learning_languages.languages.api.viewsets import LanguageViewSet
 from rest_framework import routers
 
 router = routers.SimpleRouter()
+router.register(r'languages', LanguageViewSet)
